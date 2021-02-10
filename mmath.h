5bbf0ef294ecd93957ad2540f47adb6fe15dfe99
Matthew <matthewgeorgy61@gmail.com>
2021-02-09 23:53:55 -0500

    fixed vec3_sub and vec3_dot

--- mmath.h
+++ mmath.h
@@ -133,9 +133,9 @@ vec3_t
 vec3_sub(vec3_t v1, 
          vec3_t v2)
 {
-    v1.x += v2.x;
-    v1.y += v2.y;
-    v1.z += v2.z;
+    v1.x -= v2.x;
+    v1.y -= v2.y;
+    v1.z -= v2.z;
 
     return v1;
 }
@@ -152,10 +152,10 @@ vec3_scal(vec3_t vec,
 }
 
 f32     
-vec3_dot(vec3_t a, 
-         vec3_t b)
+vec3_dot(vec3_t v1, 
+         vec3_t v2)
 {
-    return (a.x * b.x + a.y * b.y + a.z * b.z);
+    return ((v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z));
 }
 
 vec3_t      
