6106a5a92568d9263fdec30a0fff412bc9850f99
Matthew <matthewgeorgy61@gmail.com>
2021-02-14 12:04:05 -0500

    fixed alignment for #define's regarding static

--- types.h
+++ types.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-#define local static
-#define global static
-#define internal static
+#define local       static
+#define global      static
+#define internal    static
 
 typedef uint8_t     u8;
 typedef uint16_t    u16;
